f1702ed880f67d9a4037e979761a51f74cf77b5a
SATHYA VITHARANAGE <sathyamadhubhashinivithanage@gmail.com>
2026-04-24 17:13:16 +0530

    Fix running issue that comes after fixing ui
    UI Desing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,12 +18,9 @@ import LecturerSchedulePage from './pages/LecturerSchedulePage.jsx';
 import SlotCalendarPage from './pages/SlotCalendarPage.jsx';
 import NotificationsPage from './pages/NotificationsPage.jsx';
 import LecturerSlotsPage from './pages/LecturerSlotsPage.jsx';
-<<<<<<< HEAD
 import LecturerPreferencesPage from './pages/LecturerPreferencesPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import AdminLoginPage from './pages/AdminLoginPage.jsx';
-=======
->>>>>>> c387108dc87a68e56a942e6989ff39fa3c572565
 import {
   getAllAppointments,
   getStudentAppointments,
